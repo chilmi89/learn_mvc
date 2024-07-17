50df2de744593721bc9ecafed6e511f8bc2ea395
chilmi89 <achmadchilmi87@gmail.com>
2024-07-17 23:32:50 +0700

    belajar asset dan view

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,8 @@
-<h1>about page</h1>
-<!-- data diamabil dari controller -->
-<p>Nama : <?php echo $data['nama']; ?>, Profesi : <?php echo $data['profesi']; ?></p>
-<script src="" async defer></script>
+<div class="container">
+    <h1 class="text-primary mt-5">about page</h1>
+    <!-- data diamabil dari controller -->
+    <img src="<?= BASEURL; ?>/img/th.jpg" alt="" class="rounded-circle mt-3" width="200" height="200">
+    <p>Nama : <?php echo $data['nama']; ?>, Profesi : <?php echo $data['profesi']; ?></p>
+    <script src="" async defer></script>
+
+</div>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>halaman <?php echo $data['judul']; ?></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-    <body>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>halaman <?php echo $data['judul']; ?></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- mengarahkan link ke localhost -->
+    <link href="<?= BASEURL; ?>/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    
+        <div class="container">
+            <a class="navbar-brand" href="<?= BASEURL; ?>">php_mvc</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="<?= BASEURL; ?>">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= BASEURL; ?>/about">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
