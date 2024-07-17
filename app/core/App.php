a439f44fddb36b36ee630ee4139ca17f3fcebec9
chilmi89 <achmadchilmi87@gmail.com>
2024-07-17 18:03:01 +0700

    menambahkan file ht acces , function parseUrl

--- app/core/App.php
+++ app/core/App.php
@@ -3,8 +3,19 @@
 
 class App{
     public function __construct(){
-        echo "Hello World";
+        var_dump($_GET);
+        var_dump($this->parseUrl());
+    }
+    // parse url dan membersihkan / dan karakter
+    public function parseURl(){
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
+
 
 
